e787080ad4f83f86a26de02be5d5eb118b864cb8
Grant <grantchapel22@gmail.com>
2022-09-12 01:21:15 -0400

    M3 - Making players move

--- IT4080_BaseNetworkingProject-main/Assets/Scripts/GameManager.cs
+++ IT4080_BaseNetworkingProject-main/Assets/Scripts/GameManager.cs
@@ -6,9 +6,27 @@ using UnityEngine;
 //
 // This adds the serverStartType property which allows you to specify how the project
 // should be run when running through the Unity editor.
-public class GameManager : MonoBehaviour {
+public class GameManager : NetworkBehaviour {
     public NetworkCommandLine.StartModes serverStartType = NetworkCommandLine.StartModes.CHOOSE;
     private GameObject networkCmdlnObj;
+
+    private Color[] playerColors = new Color[] {
+        Color.blue,
+        Color.green,
+        Color.yellow,
+        Color.grey,
+        Color.cyan
+
+    };
+
+    private int colorIndex = 0;
+
+
+
+
+
+
+
 
 
     private void Start() {
@@ -47,4 +65,21 @@ public class GameManager : MonoBehaviour {
             NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
         GUILayout.Label("Mode: " + mode);
     }
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestNewPayerColorServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        if (!IsServer) return;
+
+        Color newColor = playerColors[colorIndex];
+        colorIndex += 1;
+        if (colorIndex > playerColors.Length - 1)
+        {
+            colorIndex = 0;
+        }
+        var po = NetworkManager.Singleton.ConnectedClients[serverRpcParams.Receive.SenderClientId].PlayerObject;
+        Player player = po.GetComponent<Player>();
+        player.PlayerColor.Value = newColor;
+    }
+
+
 }
--- IT4080_BaseNetworkingProject-main/Assets/Scripts/Player.cs
+++ IT4080_BaseNetworkingProject-main/Assets/Scripts/Player.cs
@@ -1,6 +1,69 @@
 using Unity.Netcode;
-using System;
 using UnityEngine;
 
 public class Player : NetworkBehaviour {
+
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    public NetworkVariable<Color> PlayerColor = new NetworkVariable<Color>(Color.red);
+    private GameManager _gameMgr;
+    public float movespeed = 1.0f;
+    public void Start()
+    {
+        ApplyPlayerColor();
+        PlayerColor.OnValueChanged += OnPlayerColorChanged;
+        
+    }
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            Vector3 move = calcmovement();
+            if(move.magnitude > 0)
+            {
+                RequestPostionForMovementServerRpc(move);
+            }
+        }
+        else
+        {
+            transform.position = Position.Value;
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            _gameMgr = GameObject.Find("GameManager").GetComponent<GameManager>();
+            _gameMgr.RequestNewPayerColorServerRpc();
+            Debug.Log("requested");
+        }
+    }
+   
+    Vector3 calcmovement()
+    {
+        Vector3 moveVect = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveVect *= movespeed;
+            return moveVect;
+    }
+    [ServerRpc]
+    void RequestPostionForMovementServerRpc(Vector3 movement)
+    {
+        Position.Value += movement;
+        float planesize = 5f;
+        Vector3 newPosition = Position.Value + movement;
+        newPosition.x = Mathf.Clamp(newPosition.x, planesize * -1, planesize);
+        newPosition.z = Mathf.Clamp(newPosition.z, planesize * -1, planesize);
+        Position.Value = newPosition;
+    }
+    
+    public void OnPlayerColorChanged(Color previous, Color current)
+    {
+        ApplyPlayerColor();
+
+    }
+    public void ApplyPlayerColor()
+    {
+        GetComponent<MeshRenderer>().material.color = PlayerColor.Value;
+    }
+   
 }
